742104372e47af49a74db2619b570a61eab09a72
Dimitar Hadzhiyanchev <dimitarbh@gmail.com>
2024-02-27 18:54:33 +0200

    update 27.02

---
+++ logIn/signUp/signUp.js
@@ -0,0 +1,33 @@
+document.getElementById("registration-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target)
+
+    let data = {};
+
+    formData.forEach((value, key) => {
+        data[key] = value;
+    })
+    
+    fetch('https://bookstorebe-production.up.railway.app/auth/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Registration successful:', data);
+    })
+    .catch(error => {
+        console.error('There was a problem with the registration:', error);
+    });
+
+})
+
