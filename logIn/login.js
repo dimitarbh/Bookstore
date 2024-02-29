8fe5079bd43c3bdbe79375575a8a7e25a142fb3e
Dimitar Hadzhiyanchev <dimitarbh@gmail.com>
2024-02-29 18:43:48 +0200

    troubleshooting login page

--- logIn/login.js
+++ logIn/login.js
@@ -1,30 +1,34 @@
-document.getElementById("login-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    const jsonData = {}
-
-    formData.forEach((value, key) => {
-        jsonData[key] = value;
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("login-form").addEventListener("submit", function(event) {
+        event.preventDefault();
+    
+        const formData = new FormData(event.target);
+        const jsonData = {};
+    
+        formData.forEach((value, key) => {
+            jsonData[key] = value;
+        });
+    
+        fetch('https://bookstorebe-production.up.railway.app/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(jsonData)
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(jsonData => {
+            console.log('Log In successful:', jsonData);
+            window.location.href = "../profile/profile.html"
+        })
+        .catch(error => {
+            console.error('There was a problem with the log in:', error);
+        });
     });
-
-    fetch('https://bookstorebe-production.up.railway.app/auth/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(jsonData)
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(jsonData => {
-        console.log('Log In successful:', jsonData);
-    })
-    .catch(error => {
-        console.error('There was a problem with the log in:', error);
-    });
-});
+    
+})
