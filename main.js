6304521f6c3cb720f85241ea553bca85d88f56ab
Dimitar Hadzhiyanchev <dimitarbh@gmail.com>
2024-03-06 12:55:16 +0200

    added logout button

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const logoutButton = document.getElementById("logout-button");
+
+    logoutButton.addEventListener("click", function() {
+        logout();
+    });
+
+    function logout() {
+        localStorage.removeItem('token');
+        window.location.href = "../index.html";
+    }
+
     fetch('https://bookstorebe-production.up.railway.app/auth/books')
     .then(response => {
         if (!response.ok) {
@@ -14,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function() {
         booksDiv.innerHTML = '';
 
         books.forEach(book => {
-            const bookDiv = document.createElement('div');//da go opravq s <li>
+            const bookDiv = document.createElement('div');
             bookDiv.classList.add('book');
             bookDiv.innerHTML = `<h5>${book.title}</h5>`;
             booksDiv.appendChild(bookDiv);
@@ -24,5 +35,6 @@ document.addEventListener("DOMContentLoaded", function() {
         console.error('There was a problem fetching the books:', error);
     });
 });
+
 //shte polzvam idto za topseller s loop , loopvam topselleing key i taka shte gi izkaram gore 3te ili edin po edin gi loop
 //templates handlebars da izpolzvam nego za knigite i tqh -> mustache
