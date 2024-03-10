400aebc27207d0d904da1bb3d3512410f89a87c1
Dimitar Hadzhiyanchev <dimitarbh@gmail.com>
2024-03-10 15:49:20 +0200

    added show more

--- main.js
+++ main.js
@@ -10,24 +10,37 @@ document.addEventListener("DOMContentLoaded", function() {
         window.location.reload();
     }
 
+    function toggleAdditionalInfo(event) {
+        const target = event.target;
+        if (target.classList.contains('toggle-btn')) {
+            const book = target.closest('.book');
+            const additionalInfo = book.querySelectorAll('.additional-info');
+            additionalInfo.forEach(info => {
+                info.classList.toggle('hide');
+            });
+            target.textContent = additionalInfo[0].classList.contains('hide') ? 'Read more' : 'Read less';
+        }
+    }
+
     const source = `
         {{#each books}}
             <li class="book">
                 <img class="cover-image" src="{{image}}" alt="Book Cover">
                 <h5>{{title}}</h5>
                 <p class="author">Author: {{author}}</p>
-                <p class="description">Description: {{description}}</p>
-                <p class="publisher">Publisher: {{publisher}}</p>
-                <p class="price">Price: {{price}}</p>
+                <p class="description additional-info hide">Description: {{description}}</p>
+                <p class="publisher additional-info hide">Publisher: {{publisher}}</p>
+                <p class="price additional-info hide">Price: {{price}}</p>
                 {{#if discountedPrice}}
-                    <p class="discounted-price">Discounted Price: {{discountedPrice}}</p>
+                    <p class="discounted-price additional-info hide">Discounted Price: {{discountedPrice}}</p>
                 {{/if}}
-                <p class="top-selling">Top Selling: {{#if topSelling}}Yes{{else}}No{{/if}}</p>
-                <p class="created-at">Created At: {{createdAt}}</p>
+                <p class="top-selling additional-info hide">Top Selling: {{#if topSelling}}Yes{{else}}No{{/if}}</p>
+                <p class="created-at additional-info hide">Created At: {{createdAt}}</p>
+                <button class="toggle-btn">Read more</button>
             </li>
         {{/each}}
     `;
-
+    
     const template = Handlebars.compile(source);
 
     fetch('https://bookstorebe-production.up.railway.app/')
@@ -38,14 +51,12 @@ document.addEventListener("DOMContentLoaded", function() {
             return response.json();
         })
         .then(data => {
-            const topSellers = data.filter(book => book.topSelling);
-            const topSellersDiv = document.getElementById('topsellers');
-            topSellersDiv.innerHTML = template({ books: topSellers });
             const booksDiv = document.getElementById('books');
             booksDiv.innerHTML = template({ books: data });
+
+            booksDiv.addEventListener('click', toggleAdditionalInfo);
         })
         .catch(error => {
             console.error('There was a problem fetching the books:', error);
         });
 });
-
