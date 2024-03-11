c7309ce5ec3517b9e8d575daf865a7a06e5d43bc
Dimitar Hadzhiyanchev <dimitarbh@gmail.com>
2024-03-11 17:38:54 +0200

    fixed the layout of the books

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function() {
         {{#each books}}
             <li class="book">
                 <img class="cover-image" src="{{image}}" alt="Book Cover">
-                <h5>{{title}}</h5>
+                <h5 class="title">{{title}}</h5>
                 <p class="author">Author: {{author}}</p>
                 <p class="description additional-info hide">Description: {{description}}</p>
                 <p class="publisher additional-info hide">Publisher: {{publisher}}</p>
@@ -34,14 +34,24 @@ document.addEventListener("DOMContentLoaded", function() {
                 {{#if discountedPrice}}
                     <p class="discounted-price additional-info hide">Discounted Price: {{discountedPrice}}</p>
                 {{/if}}
-                <p class="top-selling additional-info hide">Top Selling: {{#if topSelling}}Yes{{else}}No{{/if}}</p>
-                <p class="created-at additional-info hide">Created At: {{createdAt}}</p>
-                <button id="read-more" class="toggle-btn">Read more</button>
+                <p class=" hide">Top Selling: {{#if topSelling}}Yes{{else}}No{{/if}}</p>
+                <p class="created-at additional-info hide">Created At: {{formatDate createdAt}}</p>
+                <div class="bottom-content">
+                    <button id="read-more" class="toggle-btn">Read more</button>
+                </div>
             </li>
         {{/each}}
     `;
     
     const template = Handlebars.compile(source);
+
+    Handlebars.registerHelper('formatDate', function(dateString) {
+        const date = new Date(dateString);
+        const day = date.getDate().toString().padStart(2, '0');
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const year = date.getFullYear();
+        return `${day}.${month}.${year}`;
+    });
 
     fetch('https://bookstorebe-production.up.railway.app/')
         .then(response => {
