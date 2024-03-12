ddc7948a1ab6d4ac2c0baa70796a40f1480f54f2
Dimitar Hadzhiyanchev <dimitarbh@gmail.com>
2024-03-12 17:47:01 +0200

    new products added:

--- new-products/new-products.js
+++ new-products/new-products.js
@@ -1,18 +1,73 @@
-const date = "2024-01-10T00:00:00.000+00:00";
+document.addEventListener("DOMContentLoaded", function() {
+    function isWithinLast30Or31Days(dateString) {
+        const date = new Date(dateString);
+        const currentDate = new Date();
+        const thirtyOr31DaysAgo = new Date(currentDate);
+        thirtyOr31DaysAgo.setDate(currentDate.getDate() - (currentDate.getDate() === 31 ? 30 : 31));
+        return date >= thirtyOr31DaysAgo;
+    }
 
-const date1 = date.split('T');
-console.log(date1[0]);
+    function toggleAdditionalInfo(event) {
+        const target = event.target;
+        const book = target.closest('.book');
+        if (target.classList.contains('toggle-btn')) {
+            const additionalInfo = book.querySelectorAll('.additional-info');
+            additionalInfo.forEach(info => {
+                info.classList.toggle('hide');
+            });
+            target.textContent = additionalInfo[0].classList.contains('hide') ? 'Read more' : 'Read less';
+        }
+    }
 
-let date2 =  new Date();
+    const source = `
+    {{#each books}}
+    {{#if (isWithinLast30Or31Days createdAt)}}
+        <li class="book">
+            <img class="cover-image" src="{{image}}" alt="Book Cover">
+            <h5 class="title">{{title}}</h5>
+            <p class="author">Author: {{author}}</p>
+            <p class="description additional-info hide">Description: {{description}}</p>
+            <p class="publisher additional-info hide">Publisher: {{publisher}}</p>
+            <p class="price additional-info hide">Price: {{price}}</p>
+            {{#if discountedPrice}}
+                <p class="discounted-price additional-info hide">Discounted Price: {{discountedPrice}}</p>
+            {{/if}}
+            <p class=" hide">Top Selling: {{#if topSelling}}Yes{{else}}No{{/if}}</p>
+            <p class="created-at additional-info hide">Created At: {{formatDate createdAt}}</p>
+            <div class="bottom-content">
+                <button class="toggle-btn">Read more</button>
+            </div>
+        </li>
+    {{/if}}
+{{/each}}
+`;
+    
+    const template = Handlebars.compile(source);
+    
+    Handlebars.registerHelper("formatDate", function (dateString) {
+        const date = new Date(dateString);
+        const day = date.getDate().toString().padStart(2, "0");
+        const month = (date.getMonth() + 1).toString().padStart(2, "0");
+        const year = date.getFullYear();
+        return `${day}.${month}.${year}`;
+    });
 
-function dateFormat(){
-    const Y = date2.getFullYear();
-    const M = String(date2.getMonth() + 1).padStart(2, '0');
-    const D = String(date2.getDate()).padStart(2, '0');
+    Handlebars.registerHelper("isWithinLast30Or31Days", isWithinLast30Or31Days);
 
-    return `${Y}-${M}-${D}`;
-}
-
-const finalDate = dateFormat(date2)
-
-console.log(finalDate);
+    fetch('https://bookstorebe-production.up.railway.app/books')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const last30Or31DaysBooks = data.filter(book => isWithinLast30Or31Days(book.createdAt));
+            const booksDiv = document.getElementById('books');
+            booksDiv.innerHTML = template({ books: last30Or31DaysBooks });
+            booksDiv.addEventListener('click', toggleAdditionalInfo);
+        })
+        .catch(error => {
+            console.error('There was a problem fetching the books:', error);
+        });
+});
