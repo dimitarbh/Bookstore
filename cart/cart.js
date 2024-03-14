2a02a9b1c785e306aa44e7dcb20a0dd4ea52f196
Dimitar Hadzhiyanchev <dimitarbh@gmail.com>
2024-03-14 11:23:43 +0200

    redirection from cart to login fixed

--- cart/cart.js
+++ cart/cart.js
@@ -1,8 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("DOMContentLoaded event triggered");
     const token = localStorage.getItem('token');
 
     if (!token) {
-        window.location.href = "../logIn/login.html";
+        window.location.href = "../logIn/login.html"; 
     }
 });
